39a128ca93b690b1d66ac478a7aef7c6ed121693
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-12 16:33:28 +0200

    load phases 10-12 visual system

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./phases4-6.css";
 import "./phases7-9.css";
+import "./phases10-12.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
