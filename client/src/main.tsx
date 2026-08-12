438c83ded8a0787f9db0452a4b1c6dbbf66832bf
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-12 16:13:46 +0200

    load phases 7-9 styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./phases4-6.css";
+import "./phases7-9.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
