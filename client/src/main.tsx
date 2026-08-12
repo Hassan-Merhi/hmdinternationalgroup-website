115191ccea00312dc54fe10491ee0dc71f532e25
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-12 16:49:24 +0200

    load phase 13 performance layer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./phases4-6.css";
 import "./phases7-9.css";
 import "./phases10-12.css";
+import "./phases13.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
