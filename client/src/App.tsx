934f5ec18e38e325618082be60d7fa71a87470c0
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-12 17:59:26 +0200

    Rebuild public route architecture around textile business

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,10 @@ import { SiteShell } from "@client/components/SiteShell";
 
 const HomePage = lazy(() => import("@client/pages/HomePage").then((module) => ({ default: module.HomePage })));
 const AboutPage = lazy(() => import("@client/pages/AboutPage").then((module) => ({ default: module.AboutPage })));
-const StoryPage = lazy(() => import("@client/pages/StoryPage").then((module) => ({ default: module.StoryPage })));
-const VisionPage = lazy(() => import("@client/pages/VisionPage").then((module) => ({ default: module.VisionPage })));
 const BusinessesPage = lazy(() => import("@client/pages/BusinessesPage").then((module) => ({ default: module.BusinessesPage })));
-const CompaniesPage = lazy(() => import("@client/pages/CompaniesPage").then((module) => ({ default: module.CompaniesPage })));
-const CompanyProfilePage = lazy(() => import("@client/pages/CompanyProfilePage").then((module) => ({ default: module.CompanyProfilePage })));
 const IndustriesPage = lazy(() => import("@client/pages/IndustriesPage").then((module) => ({ default: module.IndustriesPage })));
+const ProcessPage = lazy(() => import("@client/pages/ProcessPage").then((module) => ({ default: module.ProcessPage })));
+const CompanyProfilePage = lazy(() => import("@client/pages/CompanyProfilePage").then((module) => ({ default: module.CompanyProfilePage })));
 const GlobalReachPage = lazy(() => import("@client/pages/GlobalReachPage").then((module) => ({ default: module.GlobalReachPage })));
 const GalleryPage = lazy(() => import("@client/pages/GalleryPage").then((module) => ({ default: module.GalleryPage })));
 const ContactPage = lazy(() => import("@client/pages/ContactPage").then((module) => ({ default: module.ContactPage })));
@@ -29,16 +27,21 @@ export default function App() {
           <Route element={<SiteShell />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/about/story" element={<StoryPage />} />
-            <Route path="/about/vision" element={<VisionPage />} />
-            <Route path="/companies" element={<CompaniesPage />} />
-            <Route path="/companies/:slug" element={<CompanyProfilePage />} />
-            <Route path="/industries" element={<IndustriesPage />} />
-            <Route path="/global-reach" element={<GlobalReachPage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
             <Route path="/what-we-do" element={<BusinessesPage />} />
-            <Route path="/businesses" element={<Navigate to="/what-we-do" replace />} />
+            <Route path="/products" element={<IndustriesPage />} />
+            <Route path="/process" element={<ProcessPage />} />
+            <Route path="/companies/:slug" element={<CompanyProfilePage />} />
+            <Route path="/export-markets" element={<GlobalReachPage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
             <Route path="/contact" element={<ContactPage />} />
+
+            <Route path="/hmd" element={<Navigate to="/companies/hmd-international-group" replace />} />
+            <Route path="/companies" element={<Navigate to="/companies/hmd-international-group" replace />} />
+            <Route path="/industries" element={<Navigate to="/products" replace />} />
+            <Route path="/global-reach" element={<Navigate to="/export-markets" replace />} />
+            <Route path="/businesses" element={<Navigate to="/what-we-do" replace />} />
+            <Route path="/about/story" element={<Navigate to="/about" replace />} />
+            <Route path="/about/vision" element={<Navigate to="/about" replace />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
